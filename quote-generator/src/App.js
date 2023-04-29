4a95928be94feb54749e5a16a87954835ddb8148
Nic-Sevic <47717887+Nic-Sevic@users.noreply.github.com>
2023-04-29 19:58:12 +0300

    fix: updated to internal JSON fetch - still getting effect called multiple times and not sure why

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -2,84 +2,39 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 
-var random = Math.floor(Math.random() * 100);
+var random = Math.floor(Math.random() * 50);
 
 const Quote = () => {
   const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
-  const fetchQuotes = () => {
-    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-      //fetch("https://zenquotes.io/api/quotes")
+  // something is wrong here because the effect is being called 2-4 times and then fails on reload
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/Nic-Sevic/Nic-Sevic.github.io/e8f25e71ef312adb66e73d8aa85dd740bd87aeb6/quote-generator/src/quotes.json")
       .then(response => {
         return response.json()
       })
       .then(data => {
         setQuotes(data)
       })
-  };
-
-  useEffect(() => {
-    fetchQuotes()
-  }, [])
+    return () => {
+      console.log('cleanup');
+    }
+  }, []);
 
   console.log(random);
-  console.log(quotes.quotes[0].quote);
+  console.log(quotes);
+  console.log(quotes[random].q);
 
   return (
     <div id="quote-box">
-      <p id='text'>{quotes.quotes[random].quote}</p>
-      <p id='author'>{quotes.quotes[random].author}</p>
-      {/* {quotes.length > 0 && (
-        // <ul>
-        //   {data.map(datum => (
-        //     <li key={datum.id}>{datum.q}</li>
-        //   ))}
-        // </ul>
-        <ul id="text">
-          {quotes.map(quote => (
-            <li key={quote.id}>{quote.quote}</li>
-            //<li key={datum.id}>{datum.author}</li>
-          ))}
-        </ul>
-      )} */}
+      <p id='text'>{quotes[random].q}</p>
+      <p id='author'>{quotes[random].a}</p>
+      <button id='new-quote'>New Quote</button>
+      <a id='tweet-quote' href='twitter.com/intent/tweet'>Tweet Quote</a>
     </div>
   );
 }
-
-//<div id="text">text{this.state.q}</div>
-//      <div id="author">author{this.state.a}</div>
-//      <button id="new-quote">New Quote</button>
-//      <a href="" id="tweet-quote">
-//        Tweet this
-//      </a>
-
-
-
-// class Quote extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: [],
-//       id: this.props.id,
-//       q: this.props.text,
-//       a: this.props.author
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div id="quote-box">
-//         <div id="text">text{this.state.q}</div>
-//         <div id="author">author{this.state.a}</div>
-//         <button id="new-quote">New Quote</button>
-//         <a href="" id="tweet-quote">
-//           Tweet this
-//         </a>
-//       </div>
-//     );
-//   }
-// }
 
 export default Quote;
