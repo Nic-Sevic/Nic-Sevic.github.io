7a70cc31a461da0d0e8c71d1cc2fec3b2994303b
Nic-Sevic <47717887+Nic-Sevic@users.noreply.github.com>
2023-05-20 23:04:11 +0300

    feat: added onClick to refresh quote - still have issue with 2 renders per time (possibly related to React.strict) - inital render has no data

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -2,36 +2,45 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 
-var random = Math.floor(Math.random() * 50);
-
 const Quote = () => {
-  const [quotes, setQuotes] = useState([]);
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
+  console.log('running');
+  const [quotes, setQuotes] = useState([{q: 'Loading...', a: 'Loading...'}]);
+  const [quote, setQuote] = useState();
+  const [author, setAuthor] = useState();
+  var count = 0;
+  var random = Math.floor(Math.random() * quotes.length);
+  
+  function getRandom() {
+    random = Math.floor(Math.random() * quotes.length);
+    count++;
+    setQuote(quotes[random].q);
+    setAuthor(quotes[random].a);
+  }
 
-  // something is wrong here because the effect is being called 2-4 times and then fails on reload
+  console.log('render');
+  // something is wrong here because the effect is being called 2-4 times 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/Nic-Sevic/Nic-Sevic.github.io/e8f25e71ef312adb66e73d8aa85dd740bd87aeb6/quote-generator/src/quotes.json")
       .then(response => {
-        return response.json()
+        return response.json();
       })
       .then(data => {
         setQuotes(data)
       })
+      .catch(error => {
+        console.log('Error fetching quotes:', error);
+      });
     return () => {
       console.log('cleanup');
     }
-  }, []);
+  }, [count]); //but I don't need to do this again, just the render again
 
-  console.log(random);
-  console.log(quotes);
-  console.log(quotes[random].q);
-
+  // once there's a new random, why isn't this refreshing?
   return (
     <div id="quote-box">
-      <p id='text'>{quotes[random].q}</p>
-      <p id='author'>{quotes[random].a}</p>
-      <button id='new-quote'>New Quote</button>
+      <p id='text'>{quote}</p>
+      <p id='author'>{author}</p>
+      <button id='new-quote' onClick={getRandom}>New Quote</button>
       <a id='tweet-quote' href='twitter.com/intent/tweet'>Tweet Quote</a>
     </div>
   );
